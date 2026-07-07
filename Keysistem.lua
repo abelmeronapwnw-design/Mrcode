483438cc6430bc3605492ba3f978627e90759f88
abelmeronapwnw-design <abelmeronapwnw@gmail.com>
2026-07-06 19:07:59 -0600

    Update Keysistem.lua

--- Keysistem.lua
+++ Keysistem.lua
@@ -5,7 +5,7 @@
 
 -- ⚠️ COMPLETA ESTOS DOS DATOS CON LOS TUYOS ⚠️
 local GIST_ID = "d0801495daa4d6b52aa4f0f101d03946"
-local GITHUB_TOKEN = ""  -- ← AÑADE TU TOKEN AQUÍ
+local GITHUB_TOKEN = "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"  -- ← AÑADE TU TOKEN AQUÍ
 
 local function getHubContent()
     local url = "https://api.github.com/repos/abelmeronapwnw-design/Mrcode/contents/ChiperPremium"
